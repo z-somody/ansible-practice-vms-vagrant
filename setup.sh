d19d057c744d59480919064e068e64cc66ab0e7e
somody-z <131619266+z-somody@users.noreply.github.com>
2023-08-15 19:04:21 +0200

    corrected syntax error in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
-
 # Set the hostname
-
 if [ ! -z "$VM_HOSTNAME" ]; then
     hostnamectl set-hostname $VM_HOSTNAME
 fi
 
 # Update packages
-
 sudo apt update && sudo apt upgrade -y || sudo yum update -y
 
 # Install & configure UFW or firewalld
-
 if [ -x "$(command -v ufw)" ]; then
     sudo apt install -y ufw
     sudo ufw allow OpenSSH
@@ -26,20 +22,16 @@ if [ -x "$(command -v ufw)" ]; then
 fi
 
 # Harden SSH
-
-PermitRootLogin no
-PasswordAuthentication no
-" | sudo tee -a /etc/ssh/sshd_config
+echo 'PermitRootLogin no
+PasswordAuthentication no' | sudo tee -a /etc/ssh/sshd_config
 
 # Create an admin user and set up SSH access
-
-ADMIN_USER="zoli"
+ADMIN_USER="adminuser"
 sudo useradd -m -s /bin/bash $ADMIN_USER
 sudo usermod -aG sudo $ADMIN_USER
 
 # Setup SSH key for the new admin user
-
-PUBLIC_SSH_KEY="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDKQUKT3I9kvB/KHC+KIFpxzItlpvabgaU3aH3pI/vNXlUfHn++1EsvTwOBzs7FU4VgPev8e1HJ05lEKTsOCz4zIMYh9FmGPcF7NDo+ZW1fKKcL9Fa+iAdNnM0kTNnHUDDcPVR7D0rPItTptOKNEa2zZAWxZmxqx6w0ABrSfJYM1j+l6MBJg2VcGPFzmi/DMOzbYnAbhP4pnDTmveiuKa06JGcIbDb2gMzXM6AwoJ0X5dVmMfNtfottl+AUkV1Ng9Q+vPrVIRAdZyU+fW+xhSfjVKVjIT13aMMpRvpfyxyHMf39Y5bhANpSOk7R28hvEEkZxla7S/Vdi4G8gPTQpIsLm+QRYd51MXHNw+KAChQII7tmGWXxrGpMkD6yQkPWz6x8nKdAfTOk+rdkcNVecrqIiOFLEt1l9QE6QL84F/65rS7wuiV3MTSV2OOdNit3jBMUhkaLyPIql+eKfpDo0IPJ1GhXmsRCCqtwfv5oGM25oRBgFUG4+8TjgtlNmBBIU6I8SyqOMTZs5K78oBU/Cmesbryu2wDrAndhmf0M/cSzT1d3jFqJhHRfSrtrTi4WDC+UEehh4SWJdwo+D1bGo1QJ8GobPF8oucy5XvtNk1R8n89MOYaQtMnIx7YAxBa4GFsvVlmr7HQceqSWQbKAarJbmwk6KXf3ACtymDEX4Ezk4Q=="
+PUBLIC_SSH_KEY="YOUR_PUBLIC_SSH_KEY"
 sudo mkdir -p /home/$ADMIN_USER/.ssh
 echo $PUBLIC_SSH_KEY | sudo tee /home/$ADMIN_USER/.ssh/authorized_keys
 sudo chown -R $ADMIN_USER:$ADMIN_USER /home/$ADMIN_USER/.ssh
